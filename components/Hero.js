2410f632c9cc6c17dbb3164e6fa7669267feb025
Shiho Nagano <shiho@nagano.com>
2021-11-25 19:26:15 -0800

    add laptop responsive

--- components/Hero.js
+++ components/Hero.js
@@ -3,8 +3,14 @@ import Image from 'next/image';
 import styles from '../components/hero.module.css';
 import utilsStyles from '../styles/utils.module.css';
 import Menu from './menu';
+import { useMediaQuery } from 'react-responsive';
 
 export default function Hero(){
+
+  const isDesktop = useMediaQuery({
+    query: '(min-width: 1024px)'
+  })
+
   return (
 		<div className={utilsStyles.bg_black}>
         <Menu />
@@ -12,9 +18,10 @@ export default function Hero(){
           <Image className={styles.logo} width={92} height={120} src="/assets/SeattleDJ_fist.png" alt="logo"/>
         </div>
         <div className={styles.top}>
-          <h1 className={`${utilsStyles.font_white} ${styles.title}`}>Protest<br/><span className={utilsStyles.font_accent}>T</span>unes</h1>
+        {isDesktop ? <h1 className={`${utilsStyles.font_white} ${styles.title}`}>Protest <span className={utilsStyles.font_accent}>T</span>unes</h1> 
+        : <h1 className={`${utilsStyles.font_white} ${styles.title}`}>Protest<br/><span className={utilsStyles.font_accent}>T</span>unes</h1>}
         </div>
-        <p className={styles.intro}>Musician, Singer, Rapper. He just debuted as Music Artist to follow his dream. Now it’s not ‘dream’ anymore.</p>
+        {isDesktop && <h2 className={styles.intro}>Musician, Singer, Rapper. He just debuted as Music Artist to follow his dream. Now it’s not ‘dream’ anymore.</h2>}
         <div className={styles.arrows}>
           <Image width={45} height={65} src="/assets/arrows.png" alt="arrow"/>
         </div>
