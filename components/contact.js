c8304a9ee496480638570e7a181fe5dbde83b5ef
shiv-chan <kshibuya31@gmail.com>
2021-12-05 00:03:08 -0800

    remove the fragments

--- components/contact.js
+++ components/contact.js
@@ -19,48 +19,43 @@ export default function Contact() {
 	};
 
 	return (
-		<React.Fragment>
-			<section className={utilsStyles.bg_white} id="contact">
-				<div className={styles.contact_section_container}>
-					<h2>
-						<span className={utilsStyles.font_accent}>C</span>ONTACT
-					</h2>
-					<p className={styles.thank_you}>
-						Thank you for listening! Let’s connect.
-					</p>
-					<form className={styles.form}>
-						<input
-							type="text"
-							name="name"
-							placeholder="Your Name"
-							value={values.name}
-							onChange={handleOnChange}
-							required
-						/>
-						<input
-							type="email"
-							name="email"
-							placeholder="Email Address"
-							value={values.email}
-							onChange={handleOnChange}
-							required
-						/>
-						<textarea
-							name="message"
-							placeholder="Your Message"
-							value={values.message}
-							onChange={handleOnChange}
-							required
-						/>
-						<button
-							className={utilsStyles.font_white}
-							onClick={handleSubmitBtn}
-						>
-							Send Message
-						</button>
-					</form>
-				</div>
-			</section>
-		</React.Fragment>
+		<section className={utilsStyles.bg_white} id="contact">
+			<div className={styles.contact_section_container}>
+				<h2>
+					<span className={utilsStyles.font_accent}>C</span>ONTACT
+				</h2>
+				<p className={styles.thank_you}>
+					Thank you for listening! Let’s connect.
+				</p>
+				<form className={styles.form}>
+					<input
+						type="text"
+						name="name"
+						placeholder="Your Name"
+						value={values.name}
+						onChange={handleOnChange}
+						required
+					/>
+					<input
+						type="email"
+						name="email"
+						placeholder="Email Address"
+						value={values.email}
+						onChange={handleOnChange}
+						required
+					/>
+					<textarea
+						name="message"
+						placeholder="Your Message"
+						value={values.message}
+						onChange={handleOnChange}
+						required
+					/>
+					<button className={utilsStyles.font_white} onClick={handleSubmitBtn}>
+						Send Message
+					</button>
+				</form>
+			</div>
+		</section>
 	);
 }
