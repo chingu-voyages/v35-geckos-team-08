1305be17c32949b67ee3e5d3382a6669c39a5936
shiv-chan <kshibuya31@gmail.com>
2021-12-05 20:54:04 -0800

    add a validator and set a message that appears after sending a message

--- components/contact.js
+++ components/contact.js
@@ -3,6 +3,7 @@ import styles from './contact.module.css';
 import utilsStyles from '../styles/utils.module.css';
 import emailjs from 'emailjs-com';
 import randomId from 'random-id';
+import validator from 'validator';
 
 export default function Contact() {
 	const [values, setValues] = useState({
@@ -11,24 +12,71 @@ export default function Contact() {
 		email: '',
 		message: '',
 	});
+	const [postSendMessage, setPostSendMessage] = useState(null);
 
 	const handleOnChange = (e) => {
 		const { name, value } = e.target;
 		setValues({ ...values, [name]: value });
 	};
 
-	const handleSubmitBtn = (e) => {
+	const handlePostSend = (status) => {
+		if (status === 'OK') {
+			setPostSendMessage('Your message has been sent!');
+			setValues({
+				contactNumber: randomId(4, 'aA0'),
+				name: '',
+				email: '',
+				message: '',
+			});
+		} else {
+			setPostSendMessage('Failed to send...Please try again.');
+			setValues({
+				contactNumber: randomId(4, 'aA0'),
+				name: '',
+				email: '',
+				message: '',
+			});
+		}
+	};
+
+	const handleSubmitBtn = async (e) => {
 		e.preventDefault();
 
-		emailjs
-			.send(
-				'contact_service',
-				'contact_form',
-				values,
-				process.env.NEXT_PUBLIC_EMAIL_JS_USER_ID // TODO: remove NEXT_PUBLIC_ prefix at build
-			)
-			.then((res) => console.log('SUCCESS!', res.status, res.text))
-			.catch((err) => console.error('FAILED...', err));
+		const isAllFilled = await Object.values(values).every(
+			(ele) => validator.isEmpty(ele) === false
+		);
+
+		const isValidEmail = await validator.isEmail(values.email);
+
+		if (!isAllFilled) {
+			setPostSendMessage('Please fill out all items.');
+		} else if (!isValidEmail) {
+			setPostSendMessage('Please enter a valid email.');
+		} else {
+			setPostSendMessage(null);
+		}
+
+		try {
+			isAllFilled &&
+				isValidEmail &&
+				emailjs
+					.send(
+						'contact_service',
+						'contact_form',
+						values,
+						process.env.NEXT_PUBLIC_EMAIL_JS_USER_ID // TODO: remove NEXT_PUBLIC_ prefix at build
+					)
+					.then((res) => {
+						(res.status === 200) & handlePostSend('OK');
+					})
+					.catch((err) => {
+						handlePostSend('NG');
+						console.error('FAILED...', err);
+					});
+		} catch (err) {
+			handlePostSend('NG');
+			console.error('FAILED...', err);
+		}
 	};
 
 	return (
