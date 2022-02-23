a35896f8e8f2301a50cca627c3a5f97734dda82d
protesttunes <98923356+protesttunes@users.noreply.github.com>
2022-02-23 12:33:34 -0800

    Update episodeList.js

--- episodes/episodeList.js
+++ episodes/episodeList.js
@@ -42,6 +42,111 @@ const episodeList = [
 		image:
 			'https://res.cloudinary.com/dgg86wwnn/image/upload/v1640302144/Thumbnails/Ep_4_Indigenous_Resistance_Thxgiving_heznux.jpg',
 	},
+		},
+	episodeNumber: '005',
+		onAirDateTime: 'Nov 29th, 12-2am',
+		title: 'WTO Shutdown/Anti-Globalization',
+		description:
+			'Show five of protest Tunes was on music of the anti-globalization movement and the WTO protests for the 22nd anniversary of Seattle’s infamous shut down of the WTO November 30th.'
+',
+		audio:
+			'https://res.cloudinary.com/dgg86wwnn/video/upload/v1645644564/Protest_Tunes_WTO_11-29-21_uuwmtg.mp3',
+		image:
+			'https://res.cloudinary.com/dgg86wwnn/image/upload/v1640302141/Thumbnails/Ep_5_WTO_Anniversary_btn7wk.jpg',
+	},
+		episodeNumber: '006',
+		onAirDateTime: 'Dec 6th, 12-2am',
+		title: 'Palestine Solidarity',
+		description:
+			'Protest Tunes show on Palestine Solidarity for the 24th anniversary of the 1st intifada, December 8th 1987. Featuring David Rovics, Brother Ali, Lowkey, Marcel Cartier, Shadia Mansour & more!',
+		audio:
+			'https://res.cloudinary.com/dgg86wwnn/video/upload/v1645645088/Audio/Protest_Tunes_12-06-21_Palestine_negi62.mp3',
+		image:
+			'https://res.cloudinary.com/dgg86wwnn/image/upload/v1640302141/Thumbnails/Ep_6_Palestine_Solidarity_1st_intafada_ihe3of.jpg',
+				},
+					
+		episodeNumber: '007',
+		onAirDateTime: 'Dec 13, 12-2am',
+		title: 'Reproductive Justice',
+		description:
+			'This weeks' show is on reproductive justice given the current threats to Roe v. Wade in Mississippi, Texas, and the Supreme Court. Our reproductive  justice show features Alice Bag, Amanda Palmer, Ani Difranco,  Peggy Seeger, Pussy Riot, Screaming  Females, War on Women and more.',
+		audio:
+			'https://res.cloudinary.com/dgg86wwnn/video/upload/v1645645421/Audio/Protest_Tunes_12-13-21_Reproductive_Rights_zx8vgd.mp3',
+		image:
+			'https://archive.org/download/clinicdefensetraining/clinicdefensetraining.jpg',
+				
 ];
+
+episodeNumber: '008',
+		onAirDateTime: 'Dec 20th, 12-2am',
+		title: 'Xmas Protest Songs',
+		description:
+			'This week's show is on Christmas Protest Songs:  Christmas  themed songs that are also protest songs against war, poverty, consumerism and more. Featuring David Rovics, Steve Earle, Leeroy Stagger, U.S. Girls, Elton John, Macy Gray, Alice Bag, Anti Flag and more. ',
+		audio:
+			'https://res.cloudinary.com/dgg86wwnn/video/upload/v1645646233/Audio/Protest_Tunes_12-20-21_Xmas_Protest_Songs_adgwki.mp3',
+		image:
+			'https://archive.org/download/flickr-ows-20111125TreeLighting02-6407002625/6407002625_9d62be482f_o.jpg',
+];
+
+episodeNumber: '009',
+		onAirDateTime: 'Dec 27, 12-2am',
+		title: 'Best 100 Protest Songs of 2021 (part 1)',
+		description:
+			'This week's show is part 1 of 3 of our top 100 Protest Songs of 2021, AKA Molotov Hot Tracks. I’ve compiled over 1,000 protest songs released this year, these are my favorites. Featuring Ani Difranco, Ash Bricky, The Linda Lindas, Hacktivist, Marina, Pussy Riot, Prince, Soundz of the South, Jesse Jett, Ziggy Marley, Evan Greer, Solidarity Not Silence, and more.',
+		audio:
+			'https://res.cloudinary.com/dgg86wwnn/video/upload/v1645646800/Audio/Protest_Tunes_12-27-21_Best_100_Protest_Songs_of_2021_part_1_ciig8h.mp3',
+		image:
+			'https://archive.org/download/flickr-ows-20111125TreeLighting02-6407002625/6407002625_9d62be482f_o.jpg',
+];
+
+episodeNumber: '010',
+		onAirDateTime: 'Jan 3, 12-2am',
+		title: 'Best 100 Protest Songs of 2021 (part 2)',
+		description:
+			'This week's show is part 2 of 3 of our top 100 protest songs of 2021 (out of over 1,000 protest songs I compiled).  Featuring Grace Petrie, Billy Bragg, Moon Bandits, Georgia English, Debt Neglector, Midnight Oil, Hyro The Hero, Intellectual Dark Wave, Curse Ov Dialect, Ministry and more.',
+		audio:
+			'https://res.cloudinary.com/dgg86wwnn/video/upload/v1645647225/Thumbnails/Protest_Tunes_1-3-22_Best_100_Protest_Songs_of_2021_part_2_ggffnf.mp3',
+		image:
+			'https://res.cloudinary.com/dgg86wwnn/image/upload/v1640302149/Thumbnails/Ep_8_Best_of_2021_vmj5xr.jpg',
+				
+];
+
+episodeNumber: '011',
+		onAirDateTime: 'Jan 10, 12-2am',
+		title: 'Best 100 Protest Songs of 2021 (part 3)',
+		description:
+			'This week's show is part 3 of 3 of our top 100 protest songs of 2021 (out of over 1,000 protest songs I compiled).  Featuring Grace Petrie, Billy Bragg, Moon Bandits, Georgia English, Debt Neglector, Midnight Oil, Hyro The Hero, Intellectual Dark Wave, Curse Ov Dialect, Ministry and more.',
+		audio:
+			'https://res.cloudinary.com/dgg86wwnn/video/upload/v1645647897/Audio/Protest_Tunes_01-10-22_Best_of_2021_part_3_d8homm.mp3',
+		image:
+			'https://res.cloudinary.com/dgg86wwnn/image/upload/v1640302149/Thumbnails/Ep_8_Best_of_2021_vmj5xr.jpg',
+				
+];
+
+episodeNumber: '012',
+		onAirDateTime: 'Jan 17, 12-2am',
+		title: 'MLK Day',
+		description:
+			'Our MLK Day show:  Protest songs about, inspired by, that mention, or sample Dr. Martin Luther King Jr. ',
+		audio:
+			'https://res.cloudinary.com/dgg86wwnn/video/upload/v1645648295/Audio/Protest_Tunes_01-17-22_MLK_Day_uxqenm.mp3',
+		image:
+			'https://res.cloudinary.com/dgg86wwnn/image/upload/v1640302149/Thumbnails/Ep_8_Best_of_2021_vmj5xr.jpg',
+				
+];
+
+episodeNumber: '013',
+		onAirDateTime: 'Jan 24th, 12-2am',
+		title: 'Women's Marches 5th Anniversary)',
+		description:
+			'This show celebrates the 5th anniversary of the historic 2017 Women's March, with feminist protest songs of the Trump era.',
+		audio:
+			'https://res.cloudinary.com/dgg86wwnn/video/upload/v1645646800/Audio/Protest_Tunes_12-27-21_Best_100_Protest_Songs_of_2021_part_1_ciig8h.mp3',
+		image:
+			'https://res.cloudinary.com/dgg86wwnn/image/upload/v1640302149/Thumbnails/Ep_8_Best_of_2021_vmj5xr.jpg',
+
+
+
+
 
 export default episodeList;
