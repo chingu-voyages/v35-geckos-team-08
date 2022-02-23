244312d6d5d226917e0a47ca64eea6c24456a894
protesttunes <98923356+protesttunes@users.noreply.github.com>
2022-02-23 11:19:12 -0800

    Update episodeList.js

--- episodes/episodeList.js
+++ episodes/episodeList.js
@@ -1,7 +1,7 @@
 const episodeList = [
 	{
 		episodeNumber: '001',
-		onAirDateTime: 'Nov 15th, 12-2am',
+		onAirDateTime: 'Nov 1st, 12-2am',
 		title: 'Climate',
 		description:
 			'The first Protest Tunes show aired on 11/1/21 and was on climate justice, in light of the UN climate summit in Glasgow that week. I started working on this show as my demo for KBCS during the northwest heatwave this June.',
@@ -12,7 +12,7 @@ const episodeList = [
 	},
 	{
 		episodeNumber: '002',
-		onAirDateTime: 'Nov 22nd, 12-2am',
+		onAirDateTime: 'Nov 8, 12-2am',
 		title: 'Anti-War on Terror',
 		description:
 			'Protest Tunes second show was on anti-war songs from the last 20 years of the war on/of terrorism. I started working on this show during the chaotic US withdrawal from Afghanistan this August.',
@@ -23,7 +23,7 @@ const episodeList = [
 	},
 	{
 		episodeNumber: '003',
-		onAirDateTime: 'Nov 29th, 12-2am',
+		onAirDateTime: 'Nov 15th, 12-2am',
 		title: 'Occupy Wall Street',
 		description:
 			'Our third Protest Tunes show was on protest music of the Occupy movement for the 10 year anniversary of Occupy Wall Street this fall.',
@@ -31,6 +31,16 @@ const episodeList = [
 			'/',
 		image:
 			'https://res.cloudinary.com/dgg86wwnn/image/upload/v1640302143/Thumbnails/Ep_3_Occupy_raid_of_Zuccatti_park_uctzhq.jpg',
+	},
+	episodeNumber: '004',
+		onAirDateTime: 'Nov 22nd, 12-2am',
+		title: Indigenous Resistance (Thankstaking),
+		description:
+			'Protest Tunes fourth show was on decolonization, and resistance to genocide of indigenous people's of the Americas for Thanksgiving/taking and National Day of Mourning that week.',
+		audio:
+			'https://res.cloudinary.com/dgg86wwnn/video/upload/v1645643521/Audio/Protest_Tunes_Decolonization_11-22-21_dn8c6o.mp3',
+		image:
+			'https://res.cloudinary.com/dgg86wwnn/image/upload/v1640302144/Thumbnails/Ep_4_Indigenous_Resistance_Thxgiving_heznux.jpg',
 	},
 ];
 
